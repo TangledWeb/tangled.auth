b99ed24a7c2a511ad2ac8f548d9294482b63de13
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2014-02-10 13:01:40 -0800

    Add download_url to setup()

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     description='Tangled auth integration',
     long_description=open('README.rst').read(),
     url='http://tangledframework.org/',
+    download_url='https://github.com/TangledWeb/tangled.auth',
     author='Wyatt Baldwin',
     author_email='self@wyattbaldwin.com',
     packages=[
