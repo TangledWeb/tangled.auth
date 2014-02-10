36b3bbf20e0511eac152a1a65b79e122f4637e2e
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2014-02-09 17:12:07 -0800

    Back to development: 0.1a4.dev0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='tangled.auth',
-    version='0.1a3',
+    version='0.1a4.dev0',
     description='Tangled auth integration',
     long_description=open('README.rst').read(),
     url='http://tangledframework.org/',
