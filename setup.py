12d2136c0b7c5f5093ae898785fa8bcb00d678a6
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2014-08-06 18:03:20 -0700

    Prepare release 0.1a4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='tangled.auth',
-    version='0.1a4.dev0',
+    version='0.1a4',
     description='Tangled auth integration',
     long_description=open('README.rst').read(),
     url='http://tangledframework.org/',
